3434c2154b19d52cb7a545be90ae706bb51ce411
Smurfffy <alanmurphy1995@gmail.com>
2017-04-18 16:09:53 +0100

    Neural net test

--- AIAssignment_MazeGame/src/ie/gmit/sw/ai/GameRunner.java
+++ AIAssignment_MazeGame/src/ie/gmit/sw/ai/GameRunner.java
@@ -3,6 +3,11 @@ package ie.gmit.sw.ai;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
+import ie.gmit.sw.ai.nn.BackpropagationTrainer;
+import ie.gmit.sw.ai.nn.NeuralNetwork;
+import ie.gmit.sw.ai.nn.Utils;
+import ie.gmit.sw.ai.nn.activator.Activator;
 public class GameRunner implements KeyListener{
 	private static final int MAZE_DIMENSION = 100;
 	private static final int IMAGE_COUNT = 14;
@@ -17,6 +22,32 @@ public class GameRunner implements KeyListener{
     	
     	Sprite[] sprites = getSprites();
     	view.setSprites(sprites);
+    	
+    	double[][] data = { //Health, Sword, Gun, Enemies
+    			{ 2, 0, 0, 0 }, { 2, 0, 0, 1 }, { 2, 0, 1, 1 }, { 2, 0, 1, 2 }, { 2, 1, 0, 2 },
+    			{ 2, 1, 0, 1 }, { 1, 0, 0, 0 }, { 1, 0, 0, 1 }, { 1, 0, 1, 1 }, { 1, 0, 1, 2 }, 
+    			{ 1, 1, 0, 2 }, { 1, 1, 0, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 1 }, { 0, 0, 1, 1 }, 
+    			{ 0, 0, 1, 2 }, { 0, 1, 0, 2 }, { 0, 1, 0, 1 } };
+    	
+    	double[][] expected = { //Panic, Attack, Hide, Run
+    			{ 0.0, 0.0, 1.0, 0.0 }, { 0.0, 0.0, 1.0, 0.0 }, { 1.0, 0.0, 0.0, 0.0 }, { 1.0, 0.0, 0.0, 0.0 }, 
+    			{ 0.0, 0.0, 0.0, 1.0 }, { 1.0, 0.0, 0.0, 0.0 }, { 0.0, 0.0, 1.0, 0.0 }, { 0.0, 0.0, 0.0, 1.0 }, 
+    			{ 1.0, 0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0, 1.0 }, { 0.0, 0.0, 0.0, 1.0 }, { 0.0, 0.0, 0.0, 1.0 }, 
+    			{ 0.0, 0.0, 1.0, 0.0 }, { 0.0, 0.0, 0.0, 1.0 }, { 0.0, 0.0, 0.0, 1.0 }, { 0.0, 1.0, 0.0, 0.0 }, 
+    			{ 0.0, 1.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0, 1.0 } };
+    	
+    	NeuralNetwork nn = new NeuralNetwork(Activator.ActivationFunction.Sigmoid, 4, 3, 4);
+    	
+    	BackpropagationTrainer trainer = new BackpropagationTrainer(nn);
+    	trainer.train(data, expected, 0.6, 10000);
+    	
+    	int testIndex = 11;
+    	double[] result = 
+    	nn.process(data[testIndex]);
+    	for (int i = 0; i < expected[testIndex].length; i++){
+    	System.out.print(expected[testIndex][i] + ",");
+    	}
+    	System.out.println("==>" + (Utils.getMaxIndex(result) + 1));
     	
     	placePlayer();
     	
--- AIAssignment_MazeGame/src/ie/gmit/sw/ai/Sprite.java
+++ AIAssignment_MazeGame/src/ie/gmit/sw/ai/Sprite.java
@@ -8,6 +8,8 @@ public class Sprite {
 	private BufferedImage[] frames; //The set of image frames to animate
  	private int index = 0; //Initial starting index in array
  	
+ 	private double lifeForce = 500;
+ 	
 	public Sprite(String name, String... images) throws Exception{
 		this.name = name;
 		this.index = 0; //Initialise the starting index to zero
@@ -17,6 +19,23 @@ public class Sprite {
 			frames[i] = ImageIO.read(new java.io.File(images[i])); //Read in each image as a BufferedImage
 		}
 	}
+	
+	/*public double engage(double weapon, double anger){
+		NeuralNetwork nn = NeuralNetworkFactory.getInstance().getNetwork("engage");
+		double[] inputs = {weapon, anger};
+		double result = nn.process(inputs);
+		lifeForce += result;
+		
+		FIS fis = FuzzyLogicFactory.getInstance().getFIS("engage");
+		double result = fis.evaluate(weapon, anger);
+		lifeForce += result;
+	}*/
+	
+	public boolean isAlive(){
+		return lifeForce > 0;
+	}
+	
+	//Threading below
 	
 	public BufferedImage getNext(){ //Returns the next image frame
 		int idx = index;
